1b7a62cb3d2e08ef14baa6d9690407666db723bf
Vaibhav <sukalevaibhav53@gmail.com>
2024-09-14 19:57:47 +0530

    added functionality to get and set items of input fields, validation for name value

---
+++ JS/EmpPayrollRegister.js
@@ -0,0 +1,79 @@
+const nameRef = document.getElementById("emp-name");
+const nameErrorRef = document.getElementById("nameError");
+const profileRef = document.getElementsByName("profile");
+const submitBtnRef = document.getElementById("submitButton");
+const genderRef = document.getElementsByName("gender");
+const departmentRefs = document.getElementsByClassName("checkbox");
+const salaryRef = document.getElementById("salary");
+const dayRef = document.getElementById("day");
+const monthRef = document.getElementById("month");
+const yearRef = document.getElementById("year");
+const notesRef = document.getElementById("notes");
+
+const namePattern = /^[a-zA-Z\s'-]{3,}$/;
+
+function validateName(name){
+    if(!namePattern.test(name)){
+        nameErrorRef.style.display = "block";
+        nameErrorRef.textContent = "Name must be at least 3 characters long and contain only letters, spaces, hyphens, or apostrophes.";
+        return false;
+    }
+    else{
+        nameErrorRef.style.display = "none";
+        return true;
+    }
+}
+
+submitBtnRef.addEventListener("click", (e) => {
+    e.preventDefault();
+    const nameVal = nameRef.value.trim();
+
+    if(!validateName(nameVal)){
+        return;
+    }
+
+    let selectedProfile = "";
+    for (let element of profileRef) {
+
+        if (element.checked) {
+            selectedProfile = element.value;
+        }
+    }
+
+    let selectedGender = "";
+    for (let element of genderRef) {
+        if (element.checked) {
+            selectedGender = element.value;
+        }
+    }
+
+    let selectedDepartments = [];
+    for (let element of departmentRefs) {
+        if (element.checked) {
+            selectedDepartments.push(element.value);
+        }
+    }
+
+    const selectedSalary = salaryRef.value;
+    const selectedStartDate = `${dayRef.value}-${monthRef.value}-${yearRef.value}`;
+    const notesVal = notesRef.value;
+    const empDataObj = {
+        name: nameVal,
+        profile: selectedProfile,
+        gender: selectedGender,
+        departments: selectedDepartments,
+        salary: selectedSalary,
+        startDate: selectedStartDate,
+        notes: notesVal,
+    }
+    const empRecordList = JSON.parse(localStorage.getItem("empDataList"));
+
+    if (empRecordList?.length > 0) {
+    localStorage.setItem("empDataList", JSON.stringify([...empRecordList, empDataObj]));
+     } 
+  else {
+    localStorage.setItem("empDataList", JSON.stringify([empDataObj]));
+  }
+
+  console.log("Employee Data saved: ", empDataObj);
+})
