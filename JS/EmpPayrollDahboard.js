a8fe8b100da5a3bc56825760d5795764a1aab628
Vaibhav <sukalevaibhav53@gmail.com>
2024-09-18 14:16:51 +0530

    edit functionality

---
+++ JS/EmpPayrollDahboard.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+    fetchAndDisplayEmployees();
+});
+
+function fetchAndDisplayEmployees() {
+    $.ajax({
+        url: 'http://localhost:3000/employees',
+        method: 'GET',
+        success: function (data) {
+            populateEmployeeTable(data);
+        },
+        error: function (xhr, status, error) {
+            console.error('Error fetching employees:', error);
+        }
+    });
+}
+
+function populateEmployeeTable(employees) {
+    const $tableBody = $('#display tbody');
+    $tableBody.empty(); 
+
+    employees.forEach(function (employee) {
+        const $row = createEmployeeRow(employee);
+        $tableBody.append($row);
+    });
+}
+
+function createEmployeeRow(employee) {
+
+    let departmentLabels = '';
+    employee.departments.forEach(function (dept) {
+        departmentLabels += `<span class="dept-label">${dept}</span>`;
+    });
+
+    let formattedDate = employee.startDate;
+
+    const $row = `
+        <tr>
+            <td class="emp-info">
+                <img src="${employee.profile}" alt="Employee" class="emp-img" />
+                <span>${employee.name}</span>
+            </td>
+            <td>${employee.gender}</td>
+            <td>${departmentLabels}</td>
+            <td>₹ ${employee.salary}</td>
+            <td>${formattedDate}</td>
+            <td class="action-buttons">
+                <button onclick = "deleteEmployee('${employee.id}')" class="action-btn"><img src="../assets/icons8-delete-30.png" alt="Delete"/></button>
+                <button onclick = "editEmployee('${employee.id}')" class="action-btn"><img src="../assets/icons8-edit-24.png" alt="Edit"/></button>
+            </td>
+        </tr>
+    `;
+
+    return $row;
+}
+
+function deleteEmployee(id) {
+    $.ajax({
+        url: `http://localhost:3000/employees/${id}`,
+        method: 'DELETE',
+        success: function () {
+            // Remove the row from the table
+            $(`#row-${id}`).remove();
+            console.log(`Employee with ID ${id} deleted successfully.`);
+            refreshPage();
+        },
+        error: function (xhr, status, error) {
+            console.error('Error deleting employee:', error);
+        }
+    });
+}
+
+function editEmployee(id){
+    localStorage.setItem('editEmployeeId', id);
+    // window.location.href = './EmpPayrollRegister.html';
+}
+
+function refreshPage() {
+    location.reload();
+}
+
